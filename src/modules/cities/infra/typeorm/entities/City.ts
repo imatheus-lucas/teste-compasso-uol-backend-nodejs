36979f712247936b9f07eb592b1eb0e625e4100f
imatheus-lucas <matheeuslucas13@gmail.com>
2021-10-07 14:23:18 -0300

    add config

--- src/modules/cities/infra/typeorm/entities/City.ts
+++ src/modules/cities/infra/typeorm/entities/City.ts
@@ -3,34 +3,34 @@ import {
     Column,
     PrimaryGeneratedColumn,
     CreateDateColumn,
-    UpdateDateColumn
+    UpdateDateColumn,
+    ManyToOne,
+    OneToMany
 } from 'typeorm'
 import { Length, validate as classValidator } from 'class-validator'
 import HttpError from '@shared/errors/HttpError'
+import EntityValidation from '@shared/infra/typeorm/validations/EntityValidation'
+import Client from '@modules/clients/infra/typeorm/entities/Client'
 
 @Entity('cities')
-export default class City {
+export default class City extends EntityValidation {
     @PrimaryGeneratedColumn('uuid')
     id: string
-    @Length(3, 180)
-    @Column({ type: 'varchar', length: '180' })
+
+    @Length(3, 185)
+    @Column({ type: 'varchar', length: '185' })
     name: string
 
     @Length(2, 3)
     @Column({ type: 'varchar', length: '3' })
     state: string
 
+    @OneToMany(() => Client, client => client.city)
+    clients: Client[]
+
     @CreateDateColumn()
     created_at: Date
 
     @UpdateDateColumn()
     updated_at: Date
-
-    async validate(): Promise<void> {
-        const errors = await classValidator(this)
-        if (errors.length > 0) {
-            throw new HttpError('Invalid city data', 400)
-        }
-        return
-    }
 }
